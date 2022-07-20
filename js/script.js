d9666a00ef104d218ccf3efb2bc4279a760e4ca1
Yuriy Klimenko <yurriyklimenko@gmail.com>
2022-07-20 13:52:45 +0300

    current state

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+     renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+   },
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+
+ });
