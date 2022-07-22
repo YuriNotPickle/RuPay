fa36970c4b74c0e26a6804e9537a835258c13db4
Yuriy Klimenko <yurriyklimenko@gmail.com>
2022-07-23 00:22:11 +0300

    page 4 + popup

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ const swiper = new Swiper('.swiper', {
    },
 
  });
+
+// Hide down pagination
 const eyeActive = document.querySelector(".eye-active");
 const eyeInactive = document.querySelector(".eye-inactive");
 const paginationPath = document.querySelector(".swiper-pagination");
@@ -30,3 +32,14 @@ function paginationHideShow () {
 
 eyeActive.addEventListener("click", paginationHideShow);
 eyeInactive.addEventListener("click", paginationHideShow);
+
+// page 4 popup 
+const openPopup = document.querySelector('.fourth__bottom_btn');
+const closePopup = document.querySelector('.tokenization-popup__right-part_button');
+const popup = document.querySelector('.tokenization-popup');
+
+function popupHideShow () {
+   popup.classList.toggle('active');
+}
+openPopup.addEventListener("click", popupHideShow);
+closePopup.addEventListener("click", popupHideShow);
